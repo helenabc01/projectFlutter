37995595a30cb13822bd1567282ae822ab795f08
Helena Barbosa Costa <helena.costa@germinare.org.br>
2024-10-21 14:05:44 -0300

    subindoo

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:project1/ui/colors.dart';
+import 'package:project1/view/login/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: mainColor),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
---
+++ lib/ui/colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const Color mainColor = Color(0xFF1d3557);
+const Color second = Color.fromARGB(255, 145, 38, 11);
+const Color branquinho = Color(0xFFFFFFFF);
+const Color pretin = Color(0xFF000000);
